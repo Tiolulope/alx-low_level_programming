e67f6ff89c7b6c62db17a6d575ebdfb5bcffed99
Tiolulope <juliusadedeji01@gmail.com>
2022-09-15 04:54:13 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 	{
 	while (minutes <= 59)
 	{
-	mins_remainder = minutes % 10;
+	
 	hours_remainder = hours % 10;												_putchar(hours / 10 + '0');
 	_putchar(hours_remainder + '0');											_putchar(':');
 	_putchar(minutes / 10 + '0');
@@ -26,5 +26,5 @@ void jack_bauer(void)
 																}
 	hours++;
 	minutes = 0;
-	}
+}
 }
